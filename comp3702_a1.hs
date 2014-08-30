fe309cec9d59110087e298e43045247652483ac8
NoiseButcher <guy.gimpel.00@gmail.com>
2014-08-30 16:16:10 +1000

    "well proper hitler"

--- comp3702_a1.hs
+++ comp3702_a1.hs
@@ -7,6 +7,8 @@ import Numeric
 import System.Random
 import Data.Fixed
 
+--Main do loop to run the program; extracts arguments, processes inputs and sends to map evaluation
+--functions.
 main = do
         (args:xs) <- getArgs
         handle <- openFile args ReadMode
@@ -20,7 +22,70 @@ main = do
             nASV' = read nASV :: Int
         roadMap startXY endXY obstacles
 
+--PRIMARY DO STUFF FUNCTIONS.
+--Function to sample a set of points on the input map, process paths from start to finish in regards to
+--obstacle positions then initiates a UCS on the paths.
+roadMap :: [(Float, Float)] -> [(Float, Float)] -> [[(Float, Float)]] -> IO ()
+roadMap start goal obstacles = do
+                        {-Start by randomly generating an amount of points on the map = 20*number os ASVs-}
+                        let seedn = (length start * length obstacles)
+                            points = take (seedn*50) $ randomRs (1, 100) (mkStdGen (seedn)) :: [Integer]
+                            points' = map ((/100) . fromInteger) points
+                            points'' = randomPairs points'
 
+                            {-Filter points that would be in the forbidden zone-}
+                            legalPoints = filter (checkCollision obstacles) points''
+
+                            {-Begin processing paths, first check that the path to a point
+                             is possible-}
+                            failBus = paveRoads (head start : legalPoints ++ [head goal]) (legalPoints ++ [head goal]) obstacles
+                            failBus'= group $ sort failBus
+                            noRepeat = map head failBus'
+--                        print start
+--                        print goal
+--                        print failBus
+--                        mapM_ print noRepeat
+                        uCS noRepeat [] [start !! 0] (head goal)
+
+--Forms paths from the sampled points based on obstacle locations and allowable proximity.
+paveRoads :: [(Float, Float)] -> [(Float, Float)] -> [[(Float, Float)]] -> [((Float, Float), (Float, Float), Float)]
+paveRoads points1 points2 obstacles
+                | length points1 == 2 = [z]
+                | x == True = z : paveRoads points' points2' obstacles
+                | otherwise = paveRoads points' points2' obstacles
+                    where x = checkPath a b obstacles
+                          a = head points1
+                          points' = tail points1
+                          points2' = filter (/=a) points2
+                          b = nodeSniffer a (filter (/=a) points2)
+                          z = makePath a b
+
+--This is a uniform cost search. Duh.
+uCS :: [((Float, Float), (Float, Float), Float)] -> [((Float, Float), (Float, Float), Float)] -> [(Float, Float)] -> (Float, Float) -> IO()
+uCS paths output start finish =
+                if (elem finish start') == True
+                    then do
+                           let x = group . sort $ foldl breakNodes [] outputx
+                               x'= map head $ cullNodes x
+                               y = takeWhile (/= finish) $ tail x'
+                               y' = filter (pathNotContain y) outputx
+                               tCost = sum $ map pull3 y'
+                           print y'
+                           print tCost
+                    else do
+                         uCS newmap'' outputx start' finish
+                        where   cheapest = shortPath $ filter (pathContains start) paths
+                                adjacent = filter (pathContains [(backDoor start cheapest)]) paths
+                                newCosts' = map (updateCost (pull3 cheapest)) newCosts
+                                newCosts = filter (notPath [cheapest]) adjacent
+                                newmap' = filter (notPath adjacent) newmap
+                                newmap = filter (/= cheapest) paths
+                                newmap'' = newmap' ++ newCosts'
+                                outputx = cheapest : output
+                                start' = (backDoor start cheapest) : start
+                                startx = start' !! (length start' - 1)
+
+--AUXILLARY FUNCTIONS TO HANDLE VARIOUS PROCESSING TASKS FOR ASV AND MAP DATA.
 --Function to break contents of a file (assuming it has been
 --extracted into a string using hGetContents).
 stringBreaker :: (Char -> Bool) -> String -> [String]
@@ -41,7 +106,6 @@ processMap :: String -> [[(Float, Float)]]
 processMap input = map (makeConfig . words) (drop 1 out)
                         where out = stringBreaker (== '\n') input
 
-
 isCollision :: (Float, Float) -> [(Float, Float)] -> Bool
 isCollision posXY fZone
             | (( a < x) && ( x < b ) && (c < y) && (y < d)) = True
@@ -58,29 +122,6 @@ checkCollision obstacles node
                     | (notElem True x) = True
                     | otherwise = False
                         where x = map (isCollision node) obstacles
-
-roadMap :: [(Float, Float)] -> [(Float, Float)] -> [[(Float, Float)]] -> IO ()
-roadMap start goal obstacles = do
-                        {-Start by randomly generating an amount of points on the map = 20*number os ASVs-}
-                        let seedn = (length start * length obstacles)
-                            points = take (seedn*50) $ randomRs (1, 100) (mkStdGen (seedn)) :: [Integer]
-                            points' = map ((/100) . fromInteger) points
-                            points'' = randomPairs points'
-
-                            {-Filter points that would be in the forbidden zone-}
-                            legalPoints = filter (checkCollision obstacles) points''
-
-                            {-Begin processing paths, first check that the path to a point
-                             is possible-}
-                            failBus = paveRoads (head start : legalPoints ++ [head goal]) (legalPoints ++ [head goal]) obstacles
---                            failBus' = filter ((>0.06) . pull3) failBus
-                            failBus''= group $ sort failBus
-                            noRepeat = map head failBus''
---                        print start
---                        print goal
---                        print failBus
---                        mapM_ print noRepeat
-                        uCS noRepeat [] [start !! 0] (head goal)
 
 randomPairs :: [Float] -> [(Float, Float)]
 randomPairs input = case input of [] -> []
@@ -120,18 +161,6 @@ makePath pointA pointB
                 | fst pointA < fst pointB = (pointA, pointB, getCost pointA pointB)
                 | otherwise = (pointB, pointA, getCost pointA pointB)
 
-paveRoads :: [(Float, Float)] -> [(Float, Float)] -> [[(Float, Float)]] -> [((Float, Float), (Float, Float), Float)]
-paveRoads points1 points2 obstacles
-                | length points1 == 2 = [z]
-                | x == True = z : paveRoads points' points2' obstacles
-                | otherwise = paveRoads points' points2' obstacles
-                    where x = checkPath a b obstacles
-                          a = head points1
-                          points' = tail points1
-                          points2' = filter (/=a) points2
-                          b = nodeSniffer a (filter (/=a) points2)
-                          z = makePath a b
-
 ---Small expansion on the fst and snd functions to extract element
 --three of a triple.
 pull3 :: (a, b, c) -> c
@@ -146,31 +175,6 @@ pull2 (_,b,_) = b
 --one of a triple.
 pull1 :: (a, b, c) -> a
 pull1 (a,_,_) = a
-
-uCS :: [((Float, Float), (Float, Float), Float)] -> [((Float, Float), (Float, Float), Float)] -> [(Float, Float)] -> (Float, Float) -> IO()
-uCS paths output start finish =
-                if (elem finish start') == True
-                    then do
---                           print outputx
-                           let x = group . sort $ foldl breakNodes [] outputx
-                               x'= map head $ cullNodes x
-                               y = takeWhile (/= finish) $ tail x'
-                               y' = filter (pathNotContain y) outputx
-                               tCost = sum $ map pull3 y'
-                           print y'
-                           print tCost
-                    else do
-                         uCS newmap'' outputx start' finish
-                        where   cheapest = shortPath $ filter (pathContains start) paths
-                                adjacent = filter (pathContains [(backDoor start cheapest)]) paths
-                                newCosts' = map (updateCost (pull3 cheapest)) newCosts
-                                newCosts = filter (notPath [cheapest]) adjacent
-                                newmap' = filter (notPath adjacent) newmap
-                                newmap = filter (/= cheapest) paths
-                                newmap'' = newmap' ++ newCosts'
-                                outputx = cheapest : output
-                                start' = (backDoor start cheapest) : start
-                                startx = start' !! (length start' - 1)
 
 pathContains :: [(Float, Float)] -> ((Float, Float), (Float, Float), Float) -> Bool
 pathContains pointXY pathXYC
@@ -214,24 +218,6 @@ notPath outList input
             | otherwise = notPath outList' input
                 where outList' = tail outList
 
-traceBack :: [((Float, Float), (Float, Float), Float)] -> (Float, Float) -> (Float, Float) -> [((Float, Float), (Float, Float), Float)]
-traceBack nodeList target start
---                | length extraneous < 2 = [z]
-                | x == True = [z]
-                | otherwise = z : traceBack nodeList' target start'
-                    where   x = pathContains [target] z
-                            z = shortPath $ filter (pathContains [start]) nodeList
-                            start' = backDoor [start] z
-                            delim = backDoor [start'] z
-                            nodeList' = filter (/= z) nodeList
-
---hasLink :: [] -> []
---hasLink inList
---        | length x == 0 = [z]
---        | otherwise
---            where x = group $ foldl breakNodes [] inList
-
-
 breakNodes :: [(Float, Float)] -> ((Float, Float), (Float, Float), Float) -> [(Float, Float)]
 breakNodes initList node = pull1 node : pull2 node : initList
 
@@ -242,3 +228,35 @@ cullNodes inList
             | otherwise = cullNodes inList'
                 where z = head inList
                       inList' = tail inList
+
+--formConfig :: [(Float, Float)] -> (Float, Float) -> [[(Float, Float)]] -> [(Float, Float)]
+--formConfig input target obstacles
+                --See how close to obstacle a path is, then adjust the width of the config
+                --if bestMove !isCollision then make that move with the first one.
+                --bestMove if !isCollision and dist2ASV (n - 1) <= 0.05 then make that move.
+                --
+
+dist2ASV :: (Float, Float) -> (Float, Float) -> Float
+dist2ASV pointA pointB = sqrt (a**2 + b**2) where a = abs(fst pointA - fst pointB)
+                                                  b = abs(snd pointA - snd pointB)
+
+bestMove :: (Float, Float) -> (Float, Float) -> (Float, Float)
+bestMove pointA pointB
+        | (x == y) && (x == 0) = pointA
+        | x' > y' && x < 1     = (fst pointA - 0.001, snd pointA)
+        | x' > y' && x > 1     = (fst pointA + 0.001, snd pointA)
+        | x' < y' && y < 1     = (fst pointA, snd pointA - 0.001)
+        | otherwise            = (fst pointA, snd pointA + 0.001)
+            where x  = fst pointA - fst pointB
+                  y  = snd pointA - snd pointB
+                  x' = abs x
+                  y' = abs y
+
+findCentre :: [(Float, Float)] -> (Float, Float)
+findCentre config
+        | odd x == True  = config !! (x - (x + 1)/2)
+        | otherwise      = (a, b)
+                where x = length config
+                      x' = x - (x+1)/2
+                      a = {-config x/2, config x/2 + 1 -}
+                      b = {-Something to do with that ^^^^^ -}
